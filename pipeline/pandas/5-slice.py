1a87abff34335cf196bdcab8fe14a6030691c1c2
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-07 12:15:24 +0000

    correct the col name

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -4,4 +4,4 @@
 
 def slice(df):
     """ Select every 60th row"""
-    return df[['High', 'Low', 'Close', 'Volume_BTC']].iloc[::60, :]
+    return df[['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60, :]
