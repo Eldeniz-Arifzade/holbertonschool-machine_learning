24fdd527fc26f191b87d8a69e1c3ac6cdab596a1
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-07 11:19:00 +0000

    change param name

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -7,5 +7,5 @@ import pandas as pd
 def rename(df):
     """ Rename column """
     df.rename(columns={'Timestamp': 'Datetime'}, inplace=True)
-    df['Datetime'] = pd.to_datetime(df['Datetime'], units='s')
+    df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
     return df[['Datetime', 'Close']]
