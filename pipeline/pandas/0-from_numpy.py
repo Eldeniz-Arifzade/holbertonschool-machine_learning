f13cae5427add9716dcd5b8f1ed239c83b015490
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-07 10:36:01 +0000

    pep8

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-
 ''' This module will define a function for creating a df from array '''
 
-
 import pandas as pd
+
+
 def from_numpy(array):
     """ Create dataframe from numpy array """
     cols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
