19c53d2eb905f03830727ab8e64c89ac4d4799e5
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-07 12:43:41 +0000

    gitpusher

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -1,12 +1,12 @@
-    #!/usr/bin/env python3
-    """ This module will define a fill function """
+#!/usr/bin/env python3
+""" This module will define a fill function """
 
 
-    def fill(df):
-        """ Function for properly filling missing values """
-        df = df.drop(['Weighted_Price'], axis=1)
-        df['Close'] = df['Close'].fillna(method='ffill', axis=0)
-        for col in ['High', 'Low', 'Open']:
-            df[col] = df[col].fillna(df['Close'])
-        df[['Volume_(BTC)', 'Volume_(Currency)']] = df[['Volume_(BTC)', 'Volume_(Currency)']].fillna(0)
-        return df
+def fill(df):
+    """ Function for properly filling missing values """
+    df = df.drop(['Weighted_Price'], axis=1)
+    df['Close'] = df['Close'].fillna(method='ffill', axis=0)
+    for col in ['High', 'Low', 'Open']:
+        df[col] = df[col].fillna(df['Close'])
+    df[['Volume_(BTC)', 'Volume_(Currency)']] = df[['Volume_(BTC)', 'Volume_(Currency)']].fillna(0)
+    return df
