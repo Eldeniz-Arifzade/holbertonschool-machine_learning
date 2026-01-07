262607fdd0aeb3836ffaae7834dcec81e8604ba6
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-07 12:40:22 +0000

    use axis

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -5,7 +5,7 @@
 def fill(df):
     """ Function for properly filling missing values """
     df = df.drop(['Weighted_Price'], axis=1)
-    df['Close'] = df['Close'].fillna(method='ffill')
+    df['Close'] = df['Close'].fillna(method='ffill', axis=0)
     df[['High', 'Low', 'Open']] = df[['High', 'Low', 'Open']].fillna(df['Close'])
     df[['Volume_(BTC)', 'Volume_(Currency)']].fillna(value=0, inplace=True)
     return df
