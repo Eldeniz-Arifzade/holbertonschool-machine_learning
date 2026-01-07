ba695f5cd89cb1a21747582d5423c3b91f61a57e
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-07 12:38:36 +0000

    fill missing values

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This module will define a fill function """
+
+
+def fill(df):
+    """ Function for properly filling missing values """
+    df = df.drop(['Weighted_Price'], axis=1)
+    df['Close'] = df['Close'].fillna(method='ffill')
+    df[['High', 'Low', 'Open']] = df[['High', 'Low', 'Open']].fillna(df['Close'])
+    df[['Volume_(BTC)', 'Volume_(Currency)']].fillna(value=0, inplace=True)
+    return df
