9ebd011944b5c1d47761f82b4c822125a1a5048b
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-07 11:25:04 +0000

    df to array

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" From DF to np array """
+
+import pandas as pd
+
+
+def array(df):
+    """ Return last 10 rows in np array format """
+    new_df = df[['High', 'Close']].tail(10)
+    return new_df.to_numpy()
