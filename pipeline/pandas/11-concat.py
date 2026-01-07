e4e5d75602455a9189e81fa906ad3955beb78af6
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-07 13:07:05 +0000

    add keys

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Concat two df's """
+index = __import__('10-index').index
+import pandas as pd
+
+
+def concat(df1, df2):
+    """ Concat df1 and df2 """
+    df1 = index(df1)
+    df2 = index(df2)
+    df2 = df2[df2.index <= 1417411920]
+    return pd.concat([df1, df2], keys=['bitstamp', 'coinbase'])
