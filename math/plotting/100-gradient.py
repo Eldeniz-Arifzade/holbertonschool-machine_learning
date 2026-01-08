4fb5bb6f1535441faeb77de7ae883dd6cc7349d9
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-08 16:15:12 +0000

    colorbar

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Scatter plot with colorbar """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gradient():
+    """ Scatter plot with colorbar """
+    np.random.seed(5)
+
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.scatter(x, y, c=z)
+    plt.colorbar(label='elevation (m)', orientation='vertical')
+    plt.xlabel('x coordinate (m)')
+    plt.ylabel('y coordinate (m)')
+    plt.title('Mountain Elevation')
