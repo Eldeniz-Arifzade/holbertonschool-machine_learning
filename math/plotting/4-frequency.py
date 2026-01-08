1ee8289780d71f9d0f81432967326fb084606e29
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-08 09:41:56 +0000

    bins every 10 unit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -12,7 +12,8 @@ def frequency():
     plt.figure(figsize=(6.4, 4.8))
 
     # your code here
-    plt.hist(student_grades, bins=10, edgecolor='black')
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
