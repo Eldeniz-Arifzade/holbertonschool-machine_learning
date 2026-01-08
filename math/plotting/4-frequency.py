2703123bf05763f5dcd1da0c59d5f11c30a4d1b9
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-08 09:35:38 +0000

    hist graph

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Function for plotting hist graph """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """ Plot hist graph """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.bar(student_grades, bins=10, color=['black'])
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
