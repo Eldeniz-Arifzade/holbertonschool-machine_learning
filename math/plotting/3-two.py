6103eab152883523ab67220d3636053495a27188
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-08 09:29:57 +0000

    two line graphs at once

---
+++ math/plotting/3-two.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Function for plotting 2 line graphs """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """ Plot two line graphs """
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.plot(x, y1, linestyle='dashed', color='red')
+    plt.plot(x, y2, linestyle='solid', color='green')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of Radioactive Elements')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
