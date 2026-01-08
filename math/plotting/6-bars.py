fe810ae8fa3fd6f7dd7f0f6b82d71bfb1b0f4ce3
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-08 16:10:12 +0000

    pycodestyle validate

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def bars():
     """ Plot stacked bar graph """
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
     # your code here
@@ -20,8 +20,10 @@ def bars():
 
     plt.bar(x, y0, color='red', width=0.5, label='apples')
     plt.bar(x, y1, bottom=y0, color='yellow', width=0.5, label='bananas')
-    plt.bar(x, y2, bottom=y0+y1, color='#ff8000', width=0.5, label='oranges')
-    plt.bar(x, y3, bottom=y0+y1+y2, color='#ffe5b4', width=0.5, label='peaches')
+    plt.bar(x, y2, bottom=y0+y1, color='#ff8000', width=0.5,
+            label='oranges')
+    plt.bar(x, y3, bottom=y0+y1+y2, color='#ffe5b4', width=0.5,
+            label='peaches')
     plt.ylabel('Quantity of Fruit')
     plt.title('Number of Fruit per Person')
     plt.legend()
