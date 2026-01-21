fe65469a59378182e774420e1953628a2dbcfe8f
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-21 22:51:04 +0000

    gitpusher

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -5,8 +5,10 @@
 def poly_derivative(poly):
     """ Function for calculating derivative of polynomial """
     d_poly = []
+    if len(poly) == 0:
+        return None
     for idx, power in enumerate(poly):
         d_poly.append(idx * power)
     if len(d_poly) == 1:
-        return 0
+        return [0]
     return d_poly[1:]
