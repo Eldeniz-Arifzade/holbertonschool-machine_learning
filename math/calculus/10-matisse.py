8bc1cec8d5b5f56f914bc82ca0af442fc8a16948
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-21 22:43:12 +0000

    poly derivative

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Derivative of polynomial """
+
+
+def poly_derivative(poly):
+    """ Function for calculating derivative of polynomial """
+    d_poly = []
+    for idx, power in enumerate(poly):
+        d_poly.append(index * power)
+    return d_poly[1:]
