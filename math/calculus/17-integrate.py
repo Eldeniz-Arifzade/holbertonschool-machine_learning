f811852dc7b276c6b972a860e7bc749c3f366303
eldenizarifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-22 19:06:14 +0000

    gitpusher

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,7 +4,7 @@
 
 def poly_integral(poly, C=0):
     """ Function for integrating polynomial """
-    if not (isinstance(poly, list) or len(poly) == 0 or isinstance(C, int)):
+    if not (isinstance(poly, list) or len(poly) != 0 or isinstance(C, int)):
         return None
     poly_integral = [C]
     for idx, power in enumerate(poly):
