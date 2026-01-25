7acff76e75789461411bbb783b0241c6860b24a0
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-25 22:09:25 +0000

    gitpusher

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,5 +4,7 @@
 
 def matrix_transpose(matrix):
     """ Return transpose of 2D matrix """
-    new_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    new_matrix = [
+        [matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))
+    ]
     return new_matrix
