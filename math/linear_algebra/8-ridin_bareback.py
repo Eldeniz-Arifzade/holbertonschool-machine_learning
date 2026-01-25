c704d37c79ebe35fc714990e107df49f276656bc
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-25 22:50:38 +0000

    gitpusher

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -6,10 +6,9 @@ def mat_mul(mat1, mat2):
     """ Perform matrix multiplication """
     if len(mat1[0]) != len(mat2):
         return None
-    new_mat = [[0] * len(mat2[0])] * len(mat1)
+    new_mat = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
     for i in range(len(mat1)):
-        entry = 0
-        for j in range(len(mat1[0])):
-            entry += mat1[i][j] * mat2[j][i]
-        new_mat[i][j] = entry
+        for k in range(len(mat2[0])):
+            for j in range(len(mat1[0])):
+                new_mat[i][k] += mat1[i][j] * mat2[j][k]
     return new_mat
