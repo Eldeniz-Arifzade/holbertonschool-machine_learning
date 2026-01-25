0af40e80dd03984997d333794da69a0a824f2568
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-25 22:21:44 +0000

    gitpusher

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module will define a function which will concat arrays """
+
+
+def cat_arrays(arr1, arr2):
+    """ Function for concatinating arrays """
+    arr = arr1.extend(arr2)
+    return arr
