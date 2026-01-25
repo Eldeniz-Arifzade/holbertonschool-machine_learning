13d2c8db8cc176779130165a92dffaec02b06da8
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-25 23:58:24 +0000

    gitpusher

---
+++ math/linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Advanced task #2 """
+
+
+def add_matrices(mat1, mat2):
+    """ Add 2 matrices """
+    result = []
+    if type(mat1) == type(mat2) == list:
+        if len(mat1) == len(mat2):
+            for i in range(len(mat1)):
+                sub = add_matrices(mat1[i], mat2[i])
+                if sub is None:
+                    return None
+                result.append(sub)
+            return result
+        else:
+            return None
+    else:
+        return mat1 + mat2
