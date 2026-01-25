dc96de11ebdfdbb008cd15b12da6a56500101ef2
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-25 22:59:21 +0000

    gitpusher

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module will define a function named np_shape """
+
+
+def np_shape(matrix):
+    """ Calculate the shape of numpy ndim array """
+    return matrix.shape
