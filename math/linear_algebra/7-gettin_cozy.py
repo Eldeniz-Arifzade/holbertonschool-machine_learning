6166f01ae63afbbba5a76ba92913fbe447d2f11d
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-25 22:32:52 +0000

    consider mismatch caes

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,8 +5,12 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """ Concantinate two 2D matrices """
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return mat1 + mat2
     if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         mat = mat1.copy()
         for i in range(len(mat1)):
             mat[i].extend(mat2[i])
