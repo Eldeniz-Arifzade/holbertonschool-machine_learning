b2833f75e231027e92fa5cd296c0eca7fca1a387
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-25 23:14:50 +0000

    gitpusher

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module will define a function named np_matmul """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ Multiply two matrices """
+    return mat1.dot(mat2)
