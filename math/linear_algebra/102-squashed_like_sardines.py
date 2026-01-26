56769e058dd6788f4e7e01c51b9ee9042cb7a898
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-26 00:28:09 +0000

    gitpusher

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -17,16 +17,17 @@ def cat_matrices(mat1, mat2, axis=0):
 
     if axis < 0 or axis >= len(shape1) or axis >= len(shape2):
         return None
-    
+
     for i in range(len(shape1)):
         if i != axis and shape1[i] != shape2[i]:
             return None
-    
+
     if axis == 0:
         return mat1 + mat2
-    
+
     result = []
+
     for i in range(len(mat1)):
         result.append(cat_matrices(mat1[i], mat2[i], axis - 1))
-    
+
     return result
