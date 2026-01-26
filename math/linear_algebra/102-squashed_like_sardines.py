7417f62e860226ea2fee072e9fede4ab5ec59e49
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-01-26 00:25:53 +0000

    gitpusher

---
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Advanced number #3 """
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    """ Concant matrices with py """
+    def shape(matrix):
+        """ Shape of a matrix """
+        dims = []
+        if type(matrix) is list:
+            dims.append(len(matrix))
+            matrix = matrix[0]
+        return tuple(dims)
+
+    shape1 = shape(mat1)
+    shape2 = shape(mat2)
+
+    if axis < 0 or axis >= len(mat1) or axis >= len(mat2):
+        return None
+    
+    for i in range(len(shape1)):
+        if i != axis and shape1[i] != shape2[i]:
+            return None
+    
+    if axis == 0:
+        return mat1 + mat2
+    
+    result = []
+    for i in range(len(mat1)):
+        result.append(cat_matrices(mat1[i], mat2[i], axis - 1))
+    
