0b14d7fadbcced5ce23e7d77b1927dfaad1a52ac
eldeniz <eldeniz.2006.arifzade@gmail.com>
2026-02-01 10:16:34 +0000

    gitpusher

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -15,15 +15,17 @@ def determinant(matrix):
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError('matrix must be a list of lists')
     length = len(matrix)
+
+    for row in matrix:
+        if len(row) != length:
+            raise ValueError('matrix must be a square matrix')
+
     if length == 1:
         return matrix[0][0]
 
     if length == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
-    for row in matrix:
-        if len(row) != length:
-            raise ValueError('matrix must be a square matrix')
     D = 0
     for j in range(length):
         D += matrix[0][j] * (-1) ** j * determinant(new_matrix(matrix, j))
