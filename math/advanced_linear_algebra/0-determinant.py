0f5d05b0c662488b290a7acb9d626860f63b5f8e
eldeniz <eldeniz.2006.arifzade@gmail.com>
2026-02-01 10:15:12 +0000

    gitpusher

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -12,19 +12,19 @@ def determinant(matrix):
         ]
     if matrix == [[]]:
         return 1
-    if (isinstance(matrix, list)) and (len(matrix) != 0 and isinstance(matrix[0],list)) and (isinstance(matrix[0][0], int) or isinstance(matrix[0][0], float)):
-        length = len(matrix)
-        if length == 1:
-            return matrix[0][0]
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError('matrix must be a list of lists')
+    length = len(matrix)
+    if length == 1:
+        return matrix[0][0]
 
-        if length == 2:
-            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    if length == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
-        for row in matrix:
-            if len(row) != length:
-                raise ValueError('matrix must be a square matrix')
-        D = 0
-        for j in range(length):
-            D += matrix[0][j] * (-1) ** j * determinant(new_matrix(matrix, j))
-        return D
-    raise TypeError('matrix must be a list of lists')
+    for row in matrix:
+        if len(row) != length:
+            raise ValueError('matrix must be a square matrix')
+    D = 0
+    for j in range(length):
+        D += matrix[0][j] * (-1) ** j * determinant(new_matrix(matrix, j))
+    return D
