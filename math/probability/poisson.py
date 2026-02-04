bc39090dddce76b5f17150c40c12f75bf9fde047
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-02-04 19:56:25 +0000

    gitpusher

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -16,3 +16,14 @@ class Poisson():
             raise ValueError('data must contain multiple values')
         else:
             self.lambtha = sum(data) / len(data)
+    
+    def pmf(self, k):
+        """ Calculates the value of PMF for given num of success """
+        if not isinstance(k, int):
+            self.k = int(k)
+        if k > sum(data):
+            return 0
+        f = 1
+        for i in range(2, k+1):
+            f = f * i
+        return (self.lambtha ** k) * (2.7182818285 ** (-self.lambtha)) /  f
