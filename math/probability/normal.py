1b4cbc090a80609e6ca73d733592cfbb3d0456ce
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-02-05 12:56:32 +0000

    fix std

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -20,4 +20,4 @@ class Normal():
             stddev = 0
             for i in data:
                 stddev += (self.mean - i) ** 2
-            self.stddev = stddev ** (1 / 2)
+            self.stddev = stddev ** (1 / 2) / len(data)
