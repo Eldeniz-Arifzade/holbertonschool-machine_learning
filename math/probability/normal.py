d9a10ba8e65c0cdff7880fe1a10dd372dff3ee18
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-02-05 13:30:38 +0000

    add white space around operator

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -41,6 +41,6 @@ class Normal():
         """ Calculate CDF value for given x """
         pi = 3.1415926536
         x = (x - self.mean) / (self.stddev * 2 ** (1 / 2))
-        erf = (x - x ** 3 / 3 + x ** 5 / 10 - x ** 7 /42 + x ** 9 / 216)
+        erf = (x - x ** 3 / 3 + x ** 5 / 10 - x ** 7 / 42 + x ** 9 / 216)
         erf = 2 * erf / (pi ** (1 / 2))
         return (1 / 2) * (1 + erf)
