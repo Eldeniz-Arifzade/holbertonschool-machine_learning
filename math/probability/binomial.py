c2fff5243a84eb956f0e4c8984f82fee99ff35d8
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-02-05 14:22:47 +0000

    init binomial class

---
+++ math/probability/binomial.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" This module will define a class Binomial """
+
+
+class Binomial():
+    """ Class for representing binomial distribution """
+    def __init__(self, data=None, n=1, p=0.5):
+        """ Initialize class """
+        if data is None:
+            if n <= 0:
+                raise ValueError('n must be a positive value')
+            if 0 < p < 1:
+                raise ValueError('p must be greater than 0 and less than 1')
+            self.n = n
+            self.p = p
+        elif not isinstance(data, list):
+            raise TypeError('data must be a list')
+        elif len(data) < 2:
+            raise ValueError('data must contain multiple values')
+        else:
+            mean = sum(data) / len(data)
+            variance = 0
+            for trail in data:
+                variance += (trail - mean) ** 2
+            variance = variance / len(data)
+            p = 1 - variance / mean
+            n = round(mean / p)
+            p = mean / n
+            self.p = p
+            self.n = n
