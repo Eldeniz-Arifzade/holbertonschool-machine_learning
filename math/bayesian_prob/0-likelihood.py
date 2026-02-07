0389bcfddad94dec292bf625f4c0e8500f1b5932
eldeniz_arifzade <eldeniz.2006.arifzade@gmail.com>
2026-02-07 15:23:59 +0000

    gitpusher

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -11,7 +11,7 @@ def likelihood(x, n, P):
         raise ValueError('x must be an integer that is greater than or equal to 0')
     elif x > n:
         raise ValueError('x cannot be greater than n')
-    elif type(P) is not 'numpy.ndarray' or P.ndim != 1:
+    elif type(P) is not np.ndarray or P.ndim != 1:
         raise TypeError('P must be a 1D numpy.ndarray')
     elif np.max(P) > 1 or np.min(P) < 0:
         raise ValueError('All values in P must be in the range [0, 1]')
