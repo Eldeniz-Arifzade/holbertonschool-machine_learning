790314f786ced47634d9fb564bd2df0fa55f2d21
eldeniz_arifzade <eldeniz.2006.arifzade@gmail.com>
2026-02-07 15:19:09 +0000

    likelihoood func

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" This module will define a function namd likelihood """
+
+
+def likelihood(x, n, P):
+    """ Function for calculating likelihood of 
+    obtaining data given probability of side effects """
+    if n < 0 or not isinstance(n, int):
+        raise ValueError('n must be a positive integer')
+    elif x < 0 or not isinstance(x, int):
+        raise ValueError('x must be an integer that is greater than or equal to 0')
+    elif x > n:
+        raise ValueError('x cannot be greater than n')
+    elif P is not numpy.ndarray or P.ndim != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    elif np.max(P) > 1 or np.min(P) < 0:
+        raise ValueError('All values in P must be in the range [0, 1]')
+    C = np.factorial(n) / (np.factorial(x) * np.factorial(n - x))
+    return C * (P ** x) * ((1 - P) ** (n - x))
