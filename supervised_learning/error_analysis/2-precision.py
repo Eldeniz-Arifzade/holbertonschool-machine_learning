6c19c239c1110e0ef0cd69584d47a1049aed2e4b
eldeniz_arifzade <eldeniz.2006.arifzade@gmail.com>
2026-02-17 09:17:45 +0000

    precision

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Calculate precision of each class in confusion matrix """
+
+
+def precision(confusion):
+    """ Calculate precision """
+    classes = len(confusion)
+    arr = np.zeros(classes)
+    for i in range(classes):
+        tp_fp = np.sum(confusion[:, i])
+        tp = confusion[i][i]
+        arr[i] = tp / tp_fp
+    return arr
