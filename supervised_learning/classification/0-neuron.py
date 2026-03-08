9147744bf2f17edde9dda227665bed94c81d1864
eldeniz <e>
2026-03-08 18:52:02 +0000

    gitpusher

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" This module will define a class named Neuron """
+
+
+class Neuron():
+    """ Class for implementing neuron in NN """
+    def __init__(self, nx):
+        """ initialize class """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
