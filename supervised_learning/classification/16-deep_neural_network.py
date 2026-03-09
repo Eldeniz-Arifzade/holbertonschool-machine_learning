742200cbde135738babe66c9a738446198ea262f
Eldeniz <eldenizarifzade02@gmail.com>
2026-03-09 09:45:57 +0000

    gitpusher

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -3,8 +3,8 @@
 import numpy as np
 
 
-class DeepNeuralNetwork():
-    """ Class for implementing L layered NN """
+class DeepNeuralNetwork:
+    """ Deep NN """
     def __init__(self, nx, layers):
         """ Initialize class """
         # validate nx
@@ -15,28 +15,27 @@ class DeepNeuralNetwork():
             raise ValueError("nx must be a positive integer")
 
         # validate layers
-        if (not isinstance(layers, list)) or len(layers) == 0:
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
-        for nodes in layers:
-            if not isinstance(nodes, int) or nodes <= 0:
-                raise TypeError("layers must be a list of positive integers")
-
-        # attributes
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        # initialize weights (He initialization)
         prev = nx
 
-        for l in range(self.L):
-            nodes = layers[l]
+        for i, nodes in enumerate(layers):
 
-            self.weights["W{}".format(l+1)] = (
+            # validate layer size
+            if not isinstance(nodes, int) or nodes <= 0:
+                raise TypeError("layers must be a list of positive integers")
+
+            # He initialization
+            self.weights["W{}".format(i + 1)] = (
                 np.random.randn(nodes, prev) * np.sqrt(2 / prev)
             )
 
-            self.weights["b{}".format(l+1)] = np.zeros((nodes, 1))
+            # bias initialization
+            self.weights["b{}".format(i + 1)] = np.zeros((nodes, 1))
 
             prev = nodes
