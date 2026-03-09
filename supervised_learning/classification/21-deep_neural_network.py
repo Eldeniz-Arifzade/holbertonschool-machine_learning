98a025c3fa2fe5b70cfca8f84c64e52ac3ca070b
Eldeniz <eldenizarifzade02@gmail.com>
2026-03-09 11:24:12 +0000

    gitpusher

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -95,7 +95,6 @@ class DeepNeuralNetwork:
 
         return prediction, cost
 
-
     def gradient_descent(self, Y, cache, alpha=0.05):
         """Calculates one pass of gradient descent on the neural network."""
         m = Y.shape[1]
