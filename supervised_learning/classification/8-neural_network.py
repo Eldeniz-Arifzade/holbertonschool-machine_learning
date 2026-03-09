8a9f8c9e3b9d0e538822d86a69a2ab1cd6bcb536
Eldeniz <eldenizarifzade02@gmail.com>
2026-03-09 07:52:31 +0000

    gitpusher

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -6,6 +6,7 @@ import numpy as np
 class NeuralNetwork():
     """ This class will define a neural network with one hidden layer """
     def __init__(self, nx, nodes):
+        """ initialize class """
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
@@ -16,8 +17,8 @@ class NeuralNetwork():
             raise ValueError('nodes must be a positive integer')
 
         self.W1 = np.random.randn(1, nx)
-        self.b1= 0
+        self.b1= np.zeros((nodes, 1))
         self.A1 = 0
         self.W2 = np.random.randn(1, nx)
-        self.b2= 0
+        self.b2= np.zeros((nodes, 1))
         self.A2 = 0
