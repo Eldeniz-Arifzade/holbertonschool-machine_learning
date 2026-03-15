8b83e6a59c382121263ab1b527333d78436a7223
Eldeniz_A <eldeniz.2006.arifzade@gmail.com>
2026-03-15 18:25:27 +0000

    gitpusher

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-"""Save and load Keras model configuration in JSON"""
+"""Save and load Keras model configuration in JSON using only Keras"""
 import tensorflow.keras as K
-import json
 
 
 def save_config(network, filename):
     """
     Saves the model's architecture (configuration) to a JSON file.
-    
+
     Parameters:
     - network: Keras model whose configuration should be saved
     - filename: path to save the JSON file
@@ -23,17 +22,17 @@ def save_config(network, filename):
 def load_config(filename):
     """
     Loads a model from a JSON configuration file.
-    
+
     Parameters:
-    - filename: JSON file path containing model configuration
-    
+    - filename: path to the JSON file containing model configuration
+
     Returns:
     - A new Keras model with the loaded architecture (uncompiled)
     """
     # Read JSON string from file
     with open(filename, 'r') as f:
         config_json = f.read()
-    
+
     # Recreate the model from JSON
     model = K.models.model_from_json(config_json)
     return model
