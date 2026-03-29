d8fd696494e16bd4dc198d13173ee29b171397ee
Eldeniz_Arifzade <eldenizarifzade02@gmail.com>
2026-03-29 19:49:05 +0000

    gitpusher

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -4,12 +4,12 @@ import numpy as np
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """ Padding and Strides """
+    """ Perform convolution on grayscale images with padding and stride """
     m, h, w = images.shape
     kh, kw = kernel.shape
     sh, sw = stride
 
-    # -------- Determine padding --------
+    # -------- Determine padding and output size --------
     if padding == 'valid':
         ph, pw = 0, 0
         H_out = (h - kh) // sh + 1
@@ -26,7 +26,15 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         H_out = (h + 2*ph - kh) // sh + 1
         W_out = (w + 2*pw - kw) // sw + 1
 
-    # -------- Convolution --------
+    # -------- Pad images --------
+    padded = np.pad(images,
+                    ((0, 0), (ph, ph), (pw, pw)),
+                    mode='constant', constant_values=0)
+
+    # -------- Initialize output --------
+    output = np.zeros((m, H_out, W_out))
+
+    # -------- Perform convolution --------
     for i in range(H_out):
         for j in range(W_out):
             patch = padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
