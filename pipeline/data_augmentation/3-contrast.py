e38178a0bc064762cec8c767ba430750b89bf41e
Eldeniz-Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-04-11 17:09:58 +0000

    gitpusher

--- pipeline/data_augmentation/3-contrast.py
+++ pipeline/data_augmentation/3-contrast.py
@@ -6,4 +6,4 @@ import tensorflow as tf
 def rotate_image(image, lower, upper):
     """ Randomly adjust the contrast """
     image = tf.cast(image, tf.float32)
-    return tf.image.random_contrast(image, lower, upper)
+    return tf.image.random_contrast(image, lower=lower, upper=upper)
