0dbda26f42e97821f62bcaccc0f7c023f49153bd
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-05-17 18:05:34 +0000

    gitpusher

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -8,23 +8,21 @@ def pca(X, var=0.95):
     """Performs PCA on a dataset.
 
     Args:
-        X (numpy.ndarray): Matrix of shape (n, d) containing the dataset.
-            All dimensions are centered at 0.
-        var (float): Fraction of variance to preserve.
+        X (numpy.ndarray): Dataset of shape (n, d) where all features
+            are centered.
+        var (float): Fraction of variance that must be preserved.
 
     Returns:
-        numpy.ndarray: Weight matrix of shape (d, nd) where nd is the
-            number of dimensions needed to maintain the desired variance.
+        numpy.ndarray: Weight matrix of shape (d, nd).
     """
-    U, S, Vt = np.linalg.svd(X, full_matrices=False)
+    _, S, Vt = np.linalg.svd(X)
 
-    explained_variance = (S ** 2)
+    explained_variance = S ** 2
     cumulative_variance = np.cumsum(explained_variance)
-    total_variance = cumulative_variance[-1]
+    cumulative_variance /= cumulative_variance[-1]
 
-    ratio = cumulative_variance / total_variance
-    nd = np.searchsorted(ratio, var) + 1
+    nd = np.min(np.where(cumulative_variance >= var)) + 1
 
-    W = Vt[:nd].T
+    W = Vt.T[:, :nd]
 
     return W
