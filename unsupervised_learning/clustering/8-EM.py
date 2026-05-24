3ca6f8e18345dcb12ddb4703853430cfe4abcb94
Eldeniz_Arifzade <eldenizarifzade02@gmail.com>
2026-05-24 18:37:49 +0000

    gitpusher

---
+++ unsupervised_learning/clustering/8-EM.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Performs expectation maximization for a GMM"""
+
+import numpy as np
+
+initialize = __import__('4-initialize').initialize
+expectation = __import__('6-expectation').expectation
+maximization = __import__('7-maximization').maximization
+
+
+def expectation_maximization(X, k, iterations=1000,
+                             tol=1e-5, verbose=False):
+    """Performs the EM algorithm for a GMM
+
+    Args:
+        X (numpy.ndarray): shape (n, d) containing the dataset
+        k (int): number of clusters
+        iterations (int): maximum number of iterations
+        tol (float): tolerance for early stopping
+        verbose (bool): whether to print log likelihood updates
+
+    Returns:
+        tuple:
+            pi (numpy.ndarray): shape (k,) containing priors
+            m (numpy.ndarray): shape (k, d) containing means
+            S (numpy.ndarray): shape (k, d, d) covariances
+            g (numpy.ndarray): shape (k, n) posterior probabilities
+            l (float): log likelihood
+        (None, None, None, None, None): on failure
+    """
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(k, int) or k <= 0 or
+            not isinstance(iterations, int) or iterations <= 0 or
+            not isinstance(tol, float) or tol < 0 or
+            not isinstance(verbose, bool)):
+        return None, None, None, None, None
+
+    pi, m, S = initialize(X, k)
+
+    if pi is None:
+        return None, None, None, None, None
+
+    prev_l = 0
+
+    for i in range(iterations + 1):
+        g, l = expectation(X, pi, m, S)
+
+        if g is None:
+            return None, None, None, None, None
+
+        if verbose and (i % 10 == 0 or i == iterations):
+            print("Log Likelihood after {} iterations: {:.5f}"
+                  .format(i, l))
+
+        if i != 0 and abs(l - prev_l) <= tol:
+            break
+
+        if i < iterations:
+            pi, m, S = maximization(X, g)
+
+            if pi is None:
+                return None, None, None, None, None
+
+            prev_l = l
+
+    return pi, m, S, g, l
