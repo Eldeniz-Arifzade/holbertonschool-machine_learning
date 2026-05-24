c0a8be35428763d9af91e490df18977b92d6648f
Eldeniz_Arifzade <eldenizarifzade02@gmail.com>
2026-05-24 18:54:52 +0000

    gitpusher

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 """Finds the best number of clusters using BIC"""
-
 import numpy as np
-
-expectation_maximization = __import__(
-    '8-EM').expectation_maximization
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
-def BIC(X, kmin=1, kmax=None, iterations=1000,
-        tol=1e-5, verbose=False):
-    """Finds the best number of clusters using BIC
+def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """Finds the best number of clusters using BIC.
 
     Args:
         X (numpy.ndarray): shape (n, d) containing dataset
@@ -20,12 +16,7 @@ def BIC(X, kmin=1, kmax=None, iterations=1000,
         verbose (bool): verbose mode for EM
 
     Returns:
-        tuple:
-            best_k (int): best cluster count
-            best_result (tuple): (pi, m, S)
-            l (numpy.ndarray): log likelihoods
-            b (numpy.ndarray): BIC values
-        (None, None, None, None): on failure
+        tuple: best_k, best_result, l, b or (None, None, None, None) on failure
     """
     if (not isinstance(X, np.ndarray) or X.ndim != 2 or
             not isinstance(kmin, int) or kmin <= 0 or
@@ -35,35 +26,24 @@ def BIC(X, kmin=1, kmax=None, iterations=1000,
             not isinstance(tol, float) or tol < 0 or
             not isinstance(verbose, bool)):
         return None, None, None, None
-
     n, d = X.shape
-
     if kmax is None:
         kmax = n
-
     if kmin >= kmax:
         return None, None, None, None
-
     l = np.zeros(kmax - kmin + 1)
     b = np.zeros(kmax - kmin + 1)
-
     best_result = None
-
+    best_k = None
     for i, k in enumerate(range(kmin, kmax + 1)):
         pi, m, S, g, log_likelihood = expectation_maximization(
             X, k, iterations, tol, verbose)
-
         if pi is None:
             return None, None, None, None
-
         l[i] = log_likelihood
-
-        p = (k - 1) + (k * d) + (k * d * (d + 1) / 2)
-
+        p = (k - 1) + (k * d) + (k * d * (d + 1) // 2)
         b[i] = p * np.log(n) - (2 * log_likelihood)
-
         if i == 0 or b[i] < np.min(b[:i]):
             best_k = k
             best_result = (pi, m, S)
-
     return best_k, best_result, l, b
