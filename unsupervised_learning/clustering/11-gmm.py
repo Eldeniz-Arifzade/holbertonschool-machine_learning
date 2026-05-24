cbf4406ca40cd21ff2b8a0a1576c465fc43c121f
Eldeniz_Arifzade <eldenizarifzade02@gmail.com>
2026-05-24 18:59:54 +0000

    gitpusher

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
-"""Calculates a GMM from a dataset using sklearn"""
-
+"""Calculates a GMM from a dataset using sklearn."""
 import sklearn.mixture
 
 
 def gmm(X, k):
-    """Calculates a GMM from a dataset
+    """Calculate a GMM from a dataset.
 
     Args:
         X (numpy.ndarray): shape (n, d) containing the dataset
         k (int): number of clusters
 
     Returns:
-        tuple:
-            pi (numpy.ndarray): cluster priors
-            m (numpy.ndarray): centroid means
-            S (numpy.ndarray): covariance matrices
-            clss (numpy.ndarray): cluster labels
-            bic (float): BIC value for the model
+        pi: cluster priors of shape (k,)
+        m: centroid means of shape (k, d)
+        S: covariance matrices of shape (k, d, d)
+        clss: cluster indices for each data point of shape (n,)
+        bic: BIC value for the model
     """
-    gmm = sklearn.mixture.GaussianMixture(n_components=k)
-
-    gmm.fit(X)
-
-    return (gmm.weights_,
-            gmm.means_,
-            gmm.covariances_,
-            gmm.predict(X),
-            gmm.bic(X))
+    model = sklearn.mixture.GaussianMixture(n_components=k)
+    model.fit(X)
+    pi = model.weights_
+    m = model.means_
+    S = model.covariances_
+    clss = model.predict(X)
+    bic = model.bic(X)
+    return pi, m, S, clss, bic
