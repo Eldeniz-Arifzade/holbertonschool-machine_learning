768b87571419af5b05d807a1f17bcac63135b6ac
Eldeniz-Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-06-05 06:29:06 +0000

    gitpusher

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -74,15 +74,17 @@ class BayesianOptimization:
                 - Y_opt (numpy.ndarray): Shape (1,), the optimal function
                   value.
         """
+        sampled = []
         for _ in range(iterations):
             X_next, _ = self.acquisition()
 
-            already_sampled = np.any(
-                np.isclose(self.gp.X, X_next)
+            already_sampled = any(
+                np.isclose(X_next[0], s) for s in sampled
             )
             if already_sampled:
                 break
 
+            sampled.append(X_next[0])
             Y_next = self.f(X_next)
             self.gp.update(X_next, Y_next)
 
