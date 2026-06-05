9cfb2a88ecdb09b04eeab19e80e29d342faf79cf
Eldeniz-Arifzade <eldeniz.2006.arifzade@gmail.com>
2026-06-05 06:48:48 +0000

    gitpusher

---
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""Module for creating a variational autoencoder."""
+import tensorflow.keras as keras
+import tensorflow as tf
+
+
+class Sampling(keras.layers.Layer):
+    """Reparameterization sampling layer for the VAE latent space."""
+
+    def call(self, inputs):
+        """Sample from the latent distribution using the reparameterization trick.
+
+        Args:
+            inputs (tuple): (z_mean, z_log_var) tensors.
+
+        Returns:
+            tensor: Sampled latent vector z = mean + eps * exp(log_var / 2).
+        """
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.random.normal(shape=(batch, dim))
+        return z_mean + tf.exp(z_log_var / 2) * epsilon
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Create a variational autoencoder.
+
+    Args:
+        input_dims (int): Dimensions of the model input.
+        hidden_layers (list): Number of nodes for each hidden layer in the
+            encoder. The hidden layers are reversed for the decoder.
+        latent_dims (int): Dimensions of the latent space representation.
+
+    Returns:
+        tuple: (encoder, decoder, auto)
+            - encoder: outputs (z, z_mean, z_log_var)
+            - decoder: the decoder model
+            - auto: the full autoencoder model compiled with adam and
+              binary cross-entropy + KL divergence loss
+    """
+    # Encoder
+    encoder_input = keras.Input(shape=(input_dims,))
+    x = encoder_input
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    z_mean = keras.layers.Dense(latent_dims, activation=None)(x)
+    z_log_var = keras.layers.Dense(latent_dims, activation=None)(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(encoder_input, [z, z_mean, z_log_var])
+
+    # Decoder
+    decoder_input = keras.Input(shape=(latent_dims,))
+    x = decoder_input
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    decoder_output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder = keras.Model(decoder_input, decoder_output)
+
+    # Autoencoder with KL divergence loss
+    auto_input = keras.Input(shape=(input_dims,))
+    z, z_mean, z_log_var = encoder(auto_input)
+    reconstructed = decoder(z)
+    auto = keras.Model(auto_input, reconstructed)
+
+    # KL divergence loss added to the model
+    kl_loss = -0.5 * tf.reduce_sum(
+        1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=-1
+    )
+    auto.add_loss(tf.reduce_mean(kl_loss))
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
