fb3cdb20e1840aea6ff5103a5ffbbf35d1de91b7
eldenizarifzade <eldeniz.2006.arifzade@gmail.com>
2026-07-28 18:44:01 +0000

    gitpusher

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -6,15 +6,17 @@ import gensim
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
                    window=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Create and train a FastText model."""
-    return gensim.models.FastText(
+    """Create and train a Gensim FastText model."""
+    model = gensim.models.FastText(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         negative=negative,
         window=window,
-        sg=not cbow,
+        sg=0 if cbow else 1,
         epochs=epochs,
         seed=seed,
         workers=workers
     )
+
+    return model
