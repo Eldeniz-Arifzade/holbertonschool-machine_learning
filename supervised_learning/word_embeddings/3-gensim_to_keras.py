ff5ccbab00f7c2e77640d93219bf1beab626923b
eldenizarifzade <eldeniz.2006.arifzade@gmail.com>
2026-07-28 18:32:51 +0000

    gitpusher

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-"""Gensim to keras"""
+"""Converts Gensim vectors to a Keras embedding layer."""
 
 import tensorflow as tf
 
 
 def gensim_to_keras(model):
-    """
-    gensim to keras function
-    Args:
-        model: trained gensim word2vec models
-    Returns: trainable keras Embedding
-    """
-    return model.wv.get_keras_embedding(train_embeddings=False)
+    """Convert a Word2Vec model into a trainable Keras Embedding."""
+    vectors = model.wv.vectors
+
+    embedding = tf.keras.layers.Embedding(
+        input_dim=vectors.shape[0],
+        output_dim=vectors.shape[1],
+        trainable=True
+    )
+
+    embedding.build()
+    embedding.set_weights([vectors])
+
+    return embedding
