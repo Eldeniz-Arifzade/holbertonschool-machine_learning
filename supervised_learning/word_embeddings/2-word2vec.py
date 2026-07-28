a9bea3028ec60cade93b60175d3806465ee7a23d
eldenizarifzade <eldeniz.2006.arifzade@gmail.com>
2026-07-28 18:43:44 +0000

    gitpusher

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -6,15 +6,17 @@ import gensim
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Create and train a Word2Vec model."""
-    return gensim.models.Word2Vec(
+    """Create and train a Gensim Word2Vec model."""
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=not cbow,
+        sg=0 if cbow else 1,
         epochs=epochs,
         seed=seed,
         workers=workers
     )
+
+    return model
