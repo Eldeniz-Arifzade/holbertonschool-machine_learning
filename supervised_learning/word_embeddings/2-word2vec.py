654150e50c6652f6d2f21611ee33baaf6e01bb3f
eldenizarifzade <eldeniz.2006.arifzade@gmail.com>
2026-07-28 19:45:21 +0000

    gitpusher

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
-""" Train Word2Vec """
-from gensim.models import Word2Vec
+"""Trains a Gensim Word2Vec model."""
+
+import gensim
 
 
-def word2vec_model(sentences, size=100, min_count=5, window=5,
-                   negative=5, cbow=True, iterations=5, seed=0, workers=1):
-    """
-    ************************************************
-    ** creates and trains a gensim word2vec model **
-    ************************************************
-    @sentences: is a list of sentences to be trained on
-    @size: is the dimensionality of the embedding layer
-    @min_count: is the minimum number of occurrences
-                of a word for use in training
-    @window: is the maximum distance between the current
-             and predicted word within a sentence
-    @negative: is the size of negative sampling
-    @cbow: is a boolean to determine the training type;
-           True is for CBOW; False is for Skip-gram
-    @iterations: is the number of iterations to train over
-    @seed: is the seed for the random number generator
-    @workers: is the number of worker threads to train the model
+def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
+                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Create, build, and train a Gensim Word2Vec model."""
+    model = gensim.models.Word2Vec(
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=0 if cbow else 1,
+        epochs=epochs,
+        seed=seed,
+        workers=workers,
+        sorted_vocab=0
+    )
 
-    Returns: the trained model
-    """
-    return Word2Vec(sentences, size=size, min_count=min_count, window=window,
-                    negative=negative, sg=not cbow, iter=iterations,
-                    seed=seed, workers=workers)
+    model.build_vocab(sentences)
+    model.wv.sort_by_descending_frequency()
+
+    if model.negative:
+        model.make_cum_table()
+
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=epochs
+    )
+
+    return model
