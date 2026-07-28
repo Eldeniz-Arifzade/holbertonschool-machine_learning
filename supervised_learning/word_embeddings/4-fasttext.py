48454126875916b93f0d36308f09a12d76cea2c2
eldenizarifzade <eldeniz.2006.arifzade@gmail.com>
2026-07-28 18:29:36 +0000

    gitpusher

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python3
-"""fasttest technique"""
+"""Trains a Gensim FastText model."""
 
 import gensim
 
 
-def fasttext_model(sentences, size=100, min_count=5,
-                   window=5, negative=5, cbow=True,
-                   iterations=5, seed=0, workers=1):
-    """
-    fasttext using gensim
-    Args:
-        sentences: list of sentences to be trained on
-        size: dimensionality of the embedding layer
-        min_count: the minimum number of occurrences of a
-                    word for use in training
-        window: maximum distance between the current and
-                predicted word within a sentence
-        negative: the size of negative sampling
-        cbow: boolean to determine the training type; True
-              is for CBOW; False is for Skip-gram
-        iterations:  number of iterations to train over
-        seed: seed for the random number generator
-        workers: number of worker threads to train the model
-    Returns: the trained model
-    """
-    model = gensim.models.FastText(sentences, min_count=min_count,
-                                   iter=iterations, size=size,
-                                   window=window, negative=negative,
-                                   seed=seed, sg=cbow, workers=workers)
-    model.train(sentences, total_examples=model.corpus_count,
-                epochs=model.iter)
+def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Create, build, and train a FastText model."""
+    model = gensim.models.FastText(
+        vector_size=vector_size,
+        min_count=min_count,
+        negative=negative,
+        window=window,
+        sg=not cbow,
+        seed=seed,
+        workers=workers
+    )
+
+    model.build_vocab(sentences)
+
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=epochs
+    )
 
     return model
